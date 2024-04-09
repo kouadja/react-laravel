2f4a905b3488a33400b222f6a93c4f308400b9fa
kouadja <richmondkouadja03@mail.com>
2024-04-09 11:12:53 +0000

    first commit

---
+++ resources/js/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+ 
+  return (
+    <div>
+     je suis kouadja
+    </div>
+  )
+}
+
+export default App
+const container = document.getElementById("root");
+ const root = ReactDOM.createRoot(container)
+root.render(<App/>)
---
+++ webpack.mix.js
@@ -0,0 +1,4 @@
+const mix = require('laravel-mix');
+
+mix.react('resources/js/app.jsx', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css');
